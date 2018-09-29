7a98670ee4bc4a0c0355576a4abb88445f2fd979
Kanthasamy Chelliah <kanthasamyiit@gmail.com>
2018-09-28 23:08:19 -0700

    Added license info

--- NAH.f90
+++ NAH.f90
@@ -1,13 +1,27 @@
 !--------------------------------------------------------------------------------------------------------------------------
 !
-!	          Author: Kanthasamy Chelliah,
+!	          Author: Dr. Kanthasamy Chelliah,
 !
-!    Amazon, Illinois Institute of Technology
+!             Amazon & Illinois Institute of Technology
 !
-!             NAH Using DFT,ESM,BEM,SONAH
+!             Nearfield Acoustic Holography (NAH) Using DFT,ESM,BEM,SONAH
 !
 !--------------------------------------------------------------------------------------------------------------------------
 
+! Copyright (C) 2018  kanthasamy chelliah
+
+!    This program is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+
+!    This program is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+
+!    You should have received a copy of the GNU General Public License
+!    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
